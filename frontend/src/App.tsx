3e433e6babfab709b59a8ce386857a74ae3a0159
arolson101 <andrew@olsononline.org>
2024-08-18 22:44:14 -0500

    shadcn/ui dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import { useState } from 'react'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <button onClick={() => setCount(count - 1)}>-</button> {count}{' '}
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <Button onClick={() => setCount(count - 1)}>-</Button> {count}{' '}
+        <Button onClick={() => setCount(count + 1)}>+</Button>
+      </ThemeProvider>
     </>
   )
 }
