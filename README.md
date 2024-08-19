d4e2df92c62b494d35e36a8ee98bc1fb7a9f2538
Andrew Olson <andrew@olsononline.org>
2024-08-19 12:15:35 -0500

    format

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ bun run index.ts
 This project was created using `bun init` in bun v1.1.24. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
 
 # todo
+
 - [x] git init depot
 - [x] bun init
 - [x] hono
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -3,7 +3,7 @@ import { Link } from '@tanstack/react-router'
 export const Nav = () => {
   return (
     <>
-      <div className='p-2 flex gap-2'>
+      <div className='flex gap-2 p-2'>
         <Link to='/' className='[&.active]:font-bold'>
           Home
         </Link>{' '}
