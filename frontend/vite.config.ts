35d28afe580650351ec7ee8d76d8b65f94552d6b
Andrew Olson <andrew@olsononline.org>
2024-08-19 10:12:01 -0500

    use vite-tsconfig-paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,11 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@server': path.resolve(__dirname, '../server'),
-    },
-  },
+  plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
   server: {
     proxy: {
       '/api': {
