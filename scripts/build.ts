22737bc6a1dffd56b795b782c5ddd90d603f4d8d
arolson101 <andrew@olsononline.org>
2024-09-10 20:16:40 -0500

    report build results

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,5 @@
 import CopyBunPlugin from '@takinabradley/copybunplugin'
-import type { BuildConfig } from 'bun'
+import type { BuildConfig, BuildOutput } from 'bun'
 import path from 'path'
 
 const projectDir = (...paths: string[]) => path.join(import.meta.dir, '..', ...paths).replaceAll('\\', '/') + '/'
@@ -25,14 +25,39 @@ const config = {
   ],
 } satisfies Partial<BuildConfig>
 
-await Bun.build({
+function report(out: BuildOutput) {
+  console.log(`${out.success ? '✅' : '❌'} built ${out.outputs[0].path}`)
+  for (const log of out.logs) {
+    switch (log.level) {
+      case 'debug':
+        console.debug(log.message)
+        break
+      case 'info':
+        console.log(log.message)
+        break
+      case 'warning':
+        console.warn(log.message)
+        break
+      case 'error':
+        console.error(log.message)
+        break
+      case 'verbose':
+        console.log(log.message)
+        break
+    }
+  }
+}
+
+const bun = await Bun.build({
   ...config,
   entrypoints: ['./server/serve-bun.ts'],
   target: 'bun',
 })
+report(bun)
 
-await Bun.build({
+const node = await Bun.build({
   ...config,
   entrypoints: ['./server/serve-node.ts'],
   target: 'node',
 })
+report(node)
