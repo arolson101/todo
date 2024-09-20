0f1e0caa2bb00f8198cf2f74b5bbfcc9ce9282ac
Andrew Olson <andrew@olsononline.org>
2024-09-20 09:27:12 -0500

    report build results

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,10 @@
 import CopyBunPlugin from '@takinabradley/copybunplugin'
+import type { BuildConfig, BuildOutput } from 'bun'
 import path from 'path'
 
 const projectDir = (...paths: string[]) => path.join(import.meta.dir, '..', ...paths).replaceAll('\\', '/') + '/'
 
-await Bun.build({
-  entrypoints: ['./server/hono.ts'],
-  target: 'bun',
+const config = {
   external: [],
   outdir: './dist',
   minify: true,
@@ -24,4 +23,41 @@ await Bun.build({
       ],
     }),
   ],
+} satisfies Partial<BuildConfig>
+
+function report(out: BuildOutput) {
+  console.log(`${out.success ? '✅' : '❌'} built ${out.outputs[0].path}`)
+  for (const log of out.logs) {
+    switch (log.level) {
+      case 'debug':
+        console.debug(log.message)
+        break
+      case 'info':
+        console.log(log.message)
+        break
+      case 'warning':
+        console.warn(log.message)
+        break
+      case 'error':
+        console.error(log.message)
+        break
+      case 'verbose':
+        console.log(log.message)
+        break
+    }
+  }
+}
+
+const bun = await Bun.build({
+  ...config,
+  entrypoints: ['./server/serve-bun.ts'],
+  target: 'bun',
 })
+report(bun)
+
+const node = await Bun.build({
+  ...config,
+  entrypoints: ['./server/serve-node.ts'],
+  target: 'node',
+})
+report(node)
