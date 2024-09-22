ad1a05126bddc65b1951a81fd6368f3833a2682a
arolson101 <andrew@olsononline.org>
2024-09-22 10:44:55 -0500

    add compression

--- server/serve-bun.ts
+++ server/serve-bun.ts
@@ -1,6 +1,11 @@
+import { compress } from 'bun-compression'
 import { serveStatic } from 'hono/bun'
 import app from './hono'
 
+// https://github.com/oven-sh/bun/issues/1723
+// import { compress } from 'hono/compress'
+
+app.use(compress())
 app.use('*', serveStatic({ root: './public' }))
 app.use('*', serveStatic({ path: './public/index.html' }))
 
--- server/serve-node.ts
+++ server/serve-node.ts
@@ -1,7 +1,9 @@
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
+import { compress } from 'hono/compress'
 import app from './hono'
 
+app.use(compress())
 app.use('*', serveStatic({ root: './public' }))
 app.use('*', serveStatic({ path: './public/index.html' }))
 
