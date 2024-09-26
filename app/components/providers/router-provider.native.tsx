a8bee4c8f48adb932979bd8212ebc64a370c2496
arolson101 <andrew@olsononline.org>
2024-09-25 21:18:39 -0500

    fix macos by using the non-native navigation stack

--- app/components/providers/router-provider.native.tsx
+++ app/components/providers/router-provider.native.tsx
@@ -1,13 +1,11 @@
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { mapEntries } from 'radash'
 import { useColorScheme } from 'react-native'
 import type { RouteObject } from 'react-router-dom'
 import app from '~/../app.json'
 import { nameof } from '~/lib/nameof'
 import { pages } from '~/routes/_pages'
-
-const Stack = createNativeStackNavigator()
+import { NavigationStack } from '../ui/navigation-stack'
 
 function pathForRoute(route: RouteObject) {
   return route.index ? '' : route.path!
@@ -25,11 +23,15 @@ export function RouterProvider() {
 
   return (
     <NavigationContainer linking={linking} theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator initialRouteName={nameof<typeof pages>('Index')}>
+      <NavigationStack.Navigator initialRouteName={nameof<typeof pages>('Index')}>
         {Object.keys(pages).map(pageName => (
-          <Stack.Screen key={pageName} name={pageName} component={pages[pageName as keyof typeof pages].Component!} />
+          <NavigationStack.Screen
+            key={pageName}
+            name={pageName}
+            component={pages[pageName as keyof typeof pages].Component!}
+          />
         ))}
-      </Stack.Navigator>
+      </NavigationStack.Navigator>
     </NavigationContainer>
   )
 }
---
+++ app/components/ui/navigation-stack.macos.tsx
@@ -0,0 +1,4 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import 'react-native-gesture-handler'
+
+export const NavigationStack = createStackNavigator()
