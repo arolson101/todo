4ad188e80ee96bffb644fec81bd44de8631ec055
Andrew Olson <andrew@olsononline.org>
2024-10-09 10:15:10 -0500

    dnd test

---
+++ app/components/ui/sort-test.tsx
@@ -0,0 +1,69 @@
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import { restrictToVerticalAxis, restrictToWindowEdges } from '@dnd-kit/modifiers'
+import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { useState } from 'react'
+
+export function SortableItem(props: { id: string | number }) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+
+  return (
+    <li ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      {props.id}
+    </li>
+  )
+}
+
+export function SortTest() {
+  const [items, setItems] = useState([1, 2, 3])
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  )
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+      modifiers={[restrictToWindowEdges, restrictToVerticalAxis]}
+    >
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        <ul>
+          {items.map((id) => (
+            <SortableItem key={id} id={id} />
+          ))}
+        </ul>
+      </SortableContext>
+    </DndContext>
+  )
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+
+    if (active.id !== over?.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id as number)
+        const newIndex = items.indexOf(over?.id as number)
+
+        return arrayMove(items, oldIndex, newIndex)
+      })
+    }
+  }
+}
